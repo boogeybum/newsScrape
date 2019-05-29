c5a2cfce6a1d835215cf519e1863c8465f4c033c
boogeybum <45719473+boogeybum@users.noreply.github.com>
2019-05-28 22:04:38 -0500

    Added 4 children to the result.title and now returning correct title text and logging in mongodb. Only gets first title, expecting three. Also, currently nothing to prevent result duplicates recorded to database.

--- server.js
+++ server.js
@@ -56,13 +56,13 @@ app.get("/scrape", function(req, res) {
 
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
-        .children("h3")
+        .children("a").children(".ShowcasePromo").children(".ShowcasePromo-info").children("h3")
         .text();
       result.link = $(this)
         .children("a")
         .attr("href");
 
-      console.log(result);
+      // console.log(result);
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
         .then(function(dbArticle) {
